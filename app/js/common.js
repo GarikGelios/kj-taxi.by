8906ca8a37195016cfcc4590e8ab88191384070b
GarikGelios <garikgelios@gmail.com>
2019-08-31 14:15:40 +0300

    add footer, look like finish

--- app/js/common.js
+++ app/js/common.js
@@ -80,4 +80,28 @@ var focusMethod = function() {
     document.getElementById("form_name").focus();
 }
 
+//Scroll to contact
+var scroll_btn2 = document.querySelector('.scroll-to-contacts-btn');
+const form_contacts = document.getElementById('contacts');
+const y_coordinate_contacts = form_contacts.getBoundingClientRect().top + window.pageYOffset;
+const yOffset2 = -200;
+function handleButtonClick2() {
+    window.scrollTo({
+        top: y_coordinate_contacts + yOffset2,
+        behavior: 'smooth'
+    });
+}
+scroll_btn2.addEventListener('click', handleButtonClick2);
 
+//Scroll to contact
+var scroll_btn3 = document.querySelector('.scroll-to-conditions-btn');
+const form_conditions = document.getElementById('conditions');
+const y_coordinate_conditions = form_conditions.getBoundingClientRect().top + window.pageYOffset;
+const yOffset3 = -200;
+function handleButtonClick3() {
+    window.scrollTo({
+        top: y_coordinate_conditions + yOffset3,
+        behavior: 'smooth'
+    });
+}
+scroll_btn3.addEventListener('click', handleButtonClick3);
