b3d8923cd92922d0c9149be6e47b865c51d327f7
GarikGelios <garikgelios@gmail.com>
2019-10-05 19:18:48 +0300

    add const request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require(`express`);
 const bodyParser = require('body-parser');
 const mailer = require(`./nodemailer`);
+const request = require(`request`);
 const secretKey = '6LfTELwUAAAAALzWsbPhXAd3fVrNrNCJZGhVAh4v';
 const app = express();
 
