79c16c8db10388a01127d624f9a0d436857b1d35
GarikGelios <garikgelios@gmail.com>
2019-10-14 10:04:28 +0300

    add recaptcha to one post with nodemailer

--- server.js
+++ server.js
@@ -23,41 +23,7 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-app.post(``, (req, res) =>{
-    const message = {
-    from: 'KJ-taxi.by <kjgrouptaxi@gmail.com>', // sender address
-    to: `kjgrouptaxi@gmail.com`, // list of receivers
-    subject: 'New driver registration', // Subject line
-    html: `<table style="max-width:580px">
-<tr>
-    <td>${req.body.name}</td>
-    <td>${req.body.surname}</td>
-    <td>${req.body.middlename}</td>
-</tr>
-<tr>
-    <td>${req.body.birthday}</td>
-    <td>${req.body.region}</td>
-    <td>${req.body.locality}</td>
-</tr>
-<tr>
-    <td>Опыт: ${req.body.experience} лет</td>
-    <td>${req.body.carbrand}</td>
-    <td>${req.body.caryear} года</td>
-</tr>
-<tr>
-    <td colspan="2">${req.body.email}</td>
-    <td>${req.body.phone}</td>
-</tr>
-<tr>
-    <td colspan="3">${req.body.comment}</td>
-</tr>
-</table>`
 
-    };
-    mailer(message);
-    user_date = req.body;
-    res.render('success', {data: req.body});
-});
 
 app.post('', (req, res) =>{
     if(
@@ -83,11 +49,46 @@ app.post('', (req, res) =>{
             }
 
             //If successful
-            return res.json({'success': false, "msg":"Capctcha passed"});
+            // return res.json({'success': false, "msg":"Capctcha passed"});
+            app.post(``, (req, res) =>{
+                const message = {
+                from: 'KJ-taxi.by <kjgrouptaxi@gmail.com>', // sender address
+                to: `kjgrouptaxi@gmail.com`, // list of receivers
+                subject: 'New driver registration', // Subject line
+                html: `<table style="max-width:580px">
+            <tr>
+                <td>${req.body.name}</td>
+                <td>${req.body.surname}</td>
+                <td>${req.body.middlename}</td>
+            </tr>
+            <tr>
+                <td>${req.body.birthday}</td>
+                <td>${req.body.region}</td>
+                <td>${req.body.locality}</td>
+            </tr>
+            <tr>
+                <td>Опыт: ${req.body.experience} лет</td>
+                <td>${req.body.carbrand}</td>
+                <td>${req.body.caryear} года</td>
+            </tr>
+            <tr>
+                <td colspan="2">${req.body.email}</td>
+                <td>${req.body.phone}</td>
+            </tr>
+            <tr>
+                <td colspan="3">${req.body.comment}</td>
+            </tr>
+            </table>`
+            
+                };
+                mailer(message);
+                user_date = req.body;
+                res.render('success', {data: req.body});
+            });
             
             
         });
-    }
+    
 });
 
 app.listen(PORT, () => console.log(`server listening at http://localhost:3000/`));
