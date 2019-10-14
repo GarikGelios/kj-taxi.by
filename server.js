a7552bbfbea76f8a3452ecfa0f2e7663e8909e4e
GarikGelios <garikgelios@gmail.com>
2019-10-14 11:05:01 +0300

    add npm recaptcha2 (but not understand how to use)

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const bodyParser = require('body-parser');
 const mailer = require(`./nodemailer`);
 const app = express();
 
+const reCAPTCHA = require('recaptcha2');
+ 
+var recaptcha = new reCAPTCHA({
+  siteKey: '6LdoErwUAAAAAEsxizHcfWa8eXPGUSavhZrRsXLk', // retrieved during setup
+  secretKey: '6LdoErwUAAAAAIoiNAWZaaQsVgaO4LjnBAv0gNjU', // retrieved during setup
+  ssl: false // optional, defaults to true.
+             // Disable if you don't want to access
+             // the Google API via a secure connection
+});
+
 const PORT = 3000;
 
 app.use('/css', express.static(__dirname + `/app/css/`));
@@ -56,5 +66,24 @@ app.post(``, (req, res) =>{
     user_date = req.body;
     res.render('success', {data: req.body});
 });
+
+function submitForm(req, res) {
+    recaptcha.validateRequest(req)
+      .then(function(){
+        // validated and secure
+        console.log("it is True")
+        res.json({formSubmit:true})
+        
+      })
+      .catch(function(errorCodes){
+        // invalid
+        console.log("it is FALSE")
+        res.json({
+          formSubmit: false,
+          errors: recaptcha.translateErrors(errorCodes) // translate error codes to human readable text
+        });
+        
+      });
+  }
 
 app.listen(PORT, () => console.log(`server listening at http://localhost:3000/`));
